177ee67e2292967c00202ec9b8c20ee9461581de
smbh1014 <smbh1014@naver.com>
2020-06-16 17:54:16 +0900

    coin_zero first

---
+++ coin_zero.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+#define MAX 1000000000
+
+using namespace std;
+
+vector<long long> value;
+
+
+
+int main() {
+
+	int N, K;
+	int val;
+
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		cin >> val;
+		value.push_back(val);
+	}
+
+
+
+}
